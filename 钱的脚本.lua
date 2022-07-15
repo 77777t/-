418241c9da514cd33dcca435b48eae3eb8c8023c
緑-777 <102775197+77777t@users.noreply.github.com>
2022-07-15 09:05:34 +0900

    Update 钱的脚本.lua

--- 钱的脚本.lua
+++ 钱的脚本.lua
@@ -1,6 +1,10 @@
 require"natives-1627063482"
+util.toast("Hello,when using 120M重复,please move util.yield")
 function global_(global_,Value)
     memory.write_int(memory.script_global(global_),Value)
+end
+function noti (va)
+    util.yield(wait_,menu.set_menu_name(loop,"120M重复"..va.."K"))
 end
 wait_=10
 menu.slider(menu.my_root(), "util.yield",{"waitut"},"建议使用6000",1, 10000, 6000, 100,function(Value)
@@ -8,10 +12,11 @@ wait_= Value
 end)
 
 loop=menu.toggle_loop(menu.my_root(),"120M重复",{},"",function()
+    menu.show_warning(loop,CLICK_MENU,"Move the util.yield",function()end)
 global_(1964179, 1)
-util.yield(wait_,menu.set_menu_name(loop,"120M重复".."+500K"))
+noti(500)
 global_(1964179, 2)
-util.yield(wait_,menu.set_menu_name(loop,"120M重复".."+750K"))
+noti(750)
 end)
 menu.click_slider(menu.my_root(), "加钱",{},"1==500K\n2==750K\n3==120M",1, 3, 1, 1, function(Value)
     if Value == 1 then 
